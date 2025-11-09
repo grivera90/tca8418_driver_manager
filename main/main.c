b5ec0743bb18a011130875022b2c966eee044c78
grivera <gonzaloriveras90@gmail.com>
2025-11-09 19:42:40 -0300

    add new commits of bsp and update main.c

--- main/main.c
+++ main/main.c
@@ -8,11 +8,15 @@
 #include <freertos/FreeRTOS.h>
 
 #include "bsp.h"
+#include "freertos/projdefs.h"
 #include "keyboard_mgr.h"
+#include "portmacro.h"
+#include "tca9554_drv.h"
 /******************************************************************************
     Defines and constants
 ******************************************************************************/
 #define READ_KEYBOARD_TASK_PRIORITY				(tskIDLE_PRIORITY + 16)
+#define READ_IO_EXPANDER_TASK_PRIORITY			(tskIDLE_PRIORITY + 3)
 #define KEY_MATCH(_key, _code, _event)			((_key)->press_code == (_code) && (_key)->event == (_event))
 /******************************************************************************
     Data types
@@ -29,6 +33,7 @@ static const char *MODULE_NAME = "[MAIN_APP]";
 ******************************************************************************/
 static void read_keyboard_task(void *pvParameters);
 static void process_on_key(kb_mgr_key_prop_t *key_prop);
+static void read_io_expander_task(void *pvParamaters);
 /******************************************************************************
     Main loop
 ******************************************************************************/
@@ -37,6 +42,19 @@ void app_main(void)
 	
 	bsp_init();
 	bsp_gpio_expander_init();
+	
+	bsp_led_ok_set(true);
+	bsp_led_error_set(true);
+	bsp_led_alarm_set(true);
+
+	vTaskDelay(pdMS_TO_TICKS(1000));
+
+	bsp_led_ok_set(false);
+	bsp_led_error_set(false);
+	bsp_led_alarm_set(false);
+	
+	vTaskDelay(pdMS_TO_TICKS(100));
+	
 	bsp_keypad_scan_device_init();
 	
 	/* Keyboard Configs. */
@@ -47,7 +65,13 @@ void app_main(void)
                                         configMINIMAL_STACK_SIZE * 6, 
                                         NULL, 
                                         READ_KEYBOARD_TASK_PRIORITY, 
-                                        NULL);	
+                                        NULL);
+                                        
+    xTaskCreate(read_io_expander_task, "read_io_expander_task", 
+                                        configMINIMAL_STACK_SIZE * 2, 
+                                        NULL, 
+                                        READ_IO_EXPANDER_TASK_PRIORITY, 
+                                        NULL);                                        
     while (true) 
     {
         printf("Hello from app_main!\n");
@@ -90,6 +114,25 @@ static void process_on_key(kb_mgr_key_prop_t *key_prop)
 	}	
 }
 
+static void read_io_expander_task(void *pvParamaters)
+{
+	printf("%s read io expander task is running\n", MODULE_NAME);
+	
+	while(1)
+	{
+		
+		bool sw1 = bsp_read_sw_1();
+		bool sw2 = bsp_read_sw_2();
+		bool sw3 = bsp_read_sw_3();
+		
+		printf("%s sw1: %s, sw1: %s, sw1: %s\n", MODULE_NAME, 
+												(sw1 == true) ? "true":"false",
+												(sw2 == true) ? "true":"false",
+												(sw3 == true) ? "true":"false");
+		
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+}
 
 
 
